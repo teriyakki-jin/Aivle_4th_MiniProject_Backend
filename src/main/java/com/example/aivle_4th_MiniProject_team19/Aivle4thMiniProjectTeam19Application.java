34838892cb77f54e5396eec87078e966ab46ff5a
teriyakki-jin <j3485@pusan.ac.kr>
2025-12-04 14:25:44 +0900

    git init

---
+++ src/main/java/com/example/aivle_4th_MiniProject_team19/Aivle4thMiniProjectTeam19Application.java
@@ -0,0 +1,13 @@
+package com.example.aivle_4th_MiniProject_team19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aivle4thMiniProjectTeam19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aivle4thMiniProjectTeam19Application.class, args);
+	}
+
+}
